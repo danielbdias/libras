fd7484b4956e8d6ab6ce2907ef2dc7696e282fed
danielbdias <danielbdias@c7a882c7-8490-401a-ba72-53713d696d23>
2010-09-14 04:00:14 +0000

    Moved libras.neuralnetworks (Neural Network project) to trunk folder

---
+++ libras.neuralnetworks/src/libras/neuralnetworks/distancenetworks/DistanceNeuronConnectionListCollection.java
@@ -0,0 +1,15 @@
+/**
+ * Package with classes used in Distance-Based Neural Networks.
+ */
+package libras.neuralnetworks.distancenetworks;
+
+import libras.neuralnetworks.NeuronConnectionListCollection;
+
+/**
+ * Represents collection with lists of neuron connections of a distance-based neural network.
+ * @author Daniel Baptista Dias
+ */
+public class DistanceNeuronConnectionListCollection extends NeuronConnectionListCollection
+{
+
+}
